b9d5a718c4c160d49ba52510dcdb8af3331e9835
José Trejo <joseatrejos>
2019-10-01 14:28:24 -0700

    primer commit

--- informacion/ViewController.swift
+++ informacion/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var contactos : [Contacto] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        contactos.append(Contacto(nombre: "Juan", telefono: "1542789325", direccion: "calle 1 #1", correo: "juan@algo.com", foto: "foto1"))
+        contactos.append(Contacto(nombre: "Jose", telefono: "4201325478", direccion: "calle 2 #2", correo: "jose@algo.com", foto: "foto2"))
+        contactos.append(Contacto(nombre: "Joel", telefono: "5012478962", direccion: "calle 3 #3", correo: "joel@algo.com", foto: "foto3"))
+        contactos.append(Contacto(nombre: "Joaquin", telefono: "2011401287", direccion: "calle 4 #4", correo: "joaquin@algo.com", foto: "foto4"))
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contactos.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaContacto") as? CeldaContactoController
+        
+        celda?.imgContacto.image = UIImage(named: contactos[indexPath.row].foto!)
+        celda?.lblNombre.text = contactos[indexPath.row].nombre
+        celda?.lblTelefono.text = contactos[indexPath.row].telefono
+        
+        return celda!
+    }
+    
+    @IBOutlet weak var TVContactos: UITableView!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToEditar"{
+            let destino = segue.destination as? EditarContactoController
+            destino?.contacto = contactos[TVContactos.indexPathForSelectedRow!.row]
+        }
+    }
 
 }
 
